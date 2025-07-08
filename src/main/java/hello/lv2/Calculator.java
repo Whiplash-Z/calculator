a605df2ebb25d3b55d9ae90ba0a45b10aac7772b
Jackson <u_innovation@naver.com>
2025-07-08 10:57:18 +0900

    :sparkles: init - 계산기 만들기 과제 프로젝트 준비 - Lv1 ~ Lv3까지 단계별로 문제가 주어진다. - 각 레벨별로 패키지를 구분해서 과제를 수행한다.

---
+++ src/main/java/hello/lv2/Calculator.java
@@ -0,0 +1,8 @@
+package hello.lv2;
+
+public class Calculator {
+    public static void main(String[] args) {
+        System.out.println("Hello Calculator !");
+    }
+
+}
---
+++ src/main/java/hello/lv3/Calculator.java
@@ -0,0 +1,8 @@
+package hello.lv3;
+
+public class Calculator {
+    public static void main(String[] args) {
+        System.out.println("Hello Calculator !");
+    }
+
+}
